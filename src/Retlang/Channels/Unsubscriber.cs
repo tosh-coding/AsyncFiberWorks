84013c470dabeec3f0d1ddafc1a37d8d3e8ed51e
tosh <tosh.coding@gmail.com>
2023-09-20 21:36:13 +0900

    Limits a calls of Unsubscriber.Dispose to one time.

--- src/Retlang/Channels/Unsubscriber.cs
+++ src/Retlang/Channels/Unsubscriber.cs
@@ -4,7 +4,10 @@ namespace Retlang.Channels
 {
     internal class Unsubscriber: IDisposable
     {
+        private readonly object _lock = new object();
         private readonly Action<Unsubscriber> _actionUnsubscribe;
+
+        private bool _disposed;
 
         public Unsubscriber(Action<Unsubscriber> actionUnsubscribe)
         {
@@ -13,6 +16,14 @@ namespace Retlang.Channels
 
         public void Dispose()
         {
+            lock (_lock)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+                _disposed = true;
+            }
             _actionUnsubscribe(this);
         }
     }
