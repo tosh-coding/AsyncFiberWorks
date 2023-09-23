e9691d2e4e304d5556d7a0b6b59d265e62990f73
tosh <tosh.coding@gmail.com>
2023-09-23 16:08:33 +0900

    Add FallbackFiber.

---
+++ src/Retlang/Fibers/FallbackFiber.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Retlang.Core;
+
+namespace Retlang.Fibers
+{
+    /// <summary>
+    /// Switch to the default PoolFiber when the fiber ends.
+    /// </summary>
+    public class FallbackFiber : IFiber
+    {
+        private readonly object _lock = new object();
+        private readonly bool _isDisposable;
+
+        private IExecutionContext _fiber;
+        private IDisposable _disposable;
+        private Queue<Action> _switchingQueue = null;
+
+        /// <summary>
+        /// Added the ability to fall back when the fiber ends.
+        /// </summary>
+        /// <param name="fiber"></param>
+        /// <param name="disposable"></param>
+        public FallbackFiber(IExecutionContext fiber, IDisposable disposable)
+        {
+            _isDisposable = disposable != null;
+            _fiber = fiber;
+            _disposable = disposable;
+        }
+
+        /// <summary>
+        /// Wrapping never-ending fibers.
+        /// </summary>
+        /// <param name="fiber"></param>
+        public FallbackFiber(IExecutionContext fiber)
+        {
+            _isDisposable = false;
+            _fiber = fiber;
+        }
+
+        /// <summary>
+        /// Always null. This is not used because the fiber falls back.
+        /// </summary>
+        public ISubscriptionRegistry FallbackDisposer { get { return null; } }
+
+        /// <summary>
+        /// <see cref="IExecutionContext.Enqueue(Action)"/>
+        /// </summary>
+        /// <param name="action"></param>
+        public void Enqueue(Action action)
+        {
+            if (!_isDisposable)
+            {
+                _fiber.Enqueue(action);
+            }
+            else
+            {
+                lock (_lock)
+                {
+                    if (_switchingQueue != null)
+                    {
+                        _switchingQueue.Enqueue(action);
+                    }
+                    else
+                    {
+                        _fiber.Enqueue(action);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Switch to the fallback.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_isDisposable)
+            {
+                return;
+            }
+
+            lock (_lock)
+            {
+                if (_disposable == null)
+                {
+                    return;
+                }
+
+                var tmpDisposable = _disposable;
+                _disposable = null;
+                _switchingQueue = new Queue<Action>();
+
+                _fiber.Enqueue(() =>
+                {
+                    tmpDisposable.Dispose();
+                    var poolFiber = new PoolFiberSlim();
+
+                    lock (_lock)
+                    {
+                        while (_switchingQueue.Count > 0)
+                        {
+                            poolFiber.Enqueue(_switchingQueue.Dequeue());
+                        }
+                        _fiber = poolFiber;
+                        _switchingQueue = null;
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ src/RetlangTests/FallbackFiberTest.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using Retlang.Channels;
+using Retlang.Core;
+using Retlang.Fibers;
+using System.Threading;
+
+namespace RetlangTests
+{
+    [TestFixture]
+    public class FallbackFiberTest
+    {
+        [Test]
+        public void FallbackTest()
+        {
+            IFiber fallbackFiber;
+            int originalManagedThreadId;
+            {
+                var threadFiber = ThreadFiber.StartNew();
+                originalManagedThreadId = threadFiber.Thread.ManagedThreadId;
+                fallbackFiber = new FallbackFiber(threadFiber, threadFiber);
+            }
+
+            int fiberManagedThreadId1 = 0;
+            int fiberManagedThreadId3 = 0;
+
+            int counter = 0;
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+                fiberManagedThreadId1 = Thread.CurrentThread.ManagedThreadId;
+            });
+
+            fallbackFiber.Dispose();
+
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+            });
+            Thread.Sleep(20);
+
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+                fiberManagedThreadId3 = Thread.CurrentThread.ManagedThreadId;
+            });
+            Thread.Sleep(20);
+
+            Assert.AreEqual(3, counter);
+            Assert.AreEqual(originalManagedThreadId, fiberManagedThreadId1);
+            Assert.AreNotEqual(originalManagedThreadId, fiberManagedThreadId3);
+        }
+
+        [Test]
+        public void DisposeUserThreadPoolViaFiberTest()
+        {
+            var userThreadPool = UserThreadPool.StartNew(1);
+            IFiber fallbackFiber;
+            int originalManagedThreadId;
+            {
+                var poolFiber = PoolFiber.StartNew(userThreadPool, new DefaultExecutor());
+                originalManagedThreadId = userThreadPool.ThreadList[0].ManagedThreadId;
+                var unsubscriber = new Unsubscriber((_) =>
+                {
+                    poolFiber.Dispose();
+                    userThreadPool.Dispose();
+                });
+                fallbackFiber = new FallbackFiber(poolFiber, unsubscriber);
+            }
+
+            int fiberManagedThreadId1 = 0;
+            int fiberManagedThreadId3 = 0;
+
+            int counter = 0;
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+                fiberManagedThreadId1 = Thread.CurrentThread.ManagedThreadId;
+            });
+
+            fallbackFiber.Dispose();
+
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+            });
+            Thread.Sleep(20);
+
+            fallbackFiber.Enqueue(() =>
+            {
+                counter += 1;
+                fiberManagedThreadId3 = Thread.CurrentThread.ManagedThreadId;
+            });
+            Thread.Sleep(20);
+
+            Assert.AreEqual(3, counter);
+            Assert.AreEqual(originalManagedThreadId, fiberManagedThreadId1);
+            Assert.AreNotEqual(originalManagedThreadId, fiberManagedThreadId3);
+            Assert.AreEqual(ThreadState.Stopped, userThreadPool.ThreadList[0].ThreadState);
+        }
+    }
+}
