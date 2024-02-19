5fdd8810f5abae25db1bb2772c2ed9ab4241dd5e
tosh <tosh.coding@gmail.com>
2024-02-20 03:53:35 +0900

    Corrected comment.

--- src/AsyncFiberWorks/Fibers/IExecutionContextWithPossibleStoppage.cs
+++ src/AsyncFiberWorks/Fibers/IExecutionContextWithPossibleStoppage.cs
@@ -3,7 +3,7 @@ using AsyncFiberWorks.Core;
 namespace AsyncFiberWorks.Fibers
 {
     /// <summary>
-    /// Enqueues pending actions for the context of execution (thread, pool of threads, message pump, etc.)
+    /// Enqueue pending actions to the execution context.
     /// Can also register channel subscription status. Used to cancel them all at once when the fiber is destroyed.
     /// </summary>
     public interface IExecutionContextWithPossibleStoppage : ISubscriptionRegistry, IExecutionContext
--- src/AsyncFiberWorks/Fibers/IFiber.cs
+++ src/AsyncFiberWorks/Fibers/IFiber.cs
@@ -4,7 +4,7 @@ using System;
 namespace AsyncFiberWorks.Fibers
 {
     /// <summary>
-    /// Enqueues pending actions for the context of execution (thread, pool of threads, message pump, etc.)
+    /// Enqueue pending actions to the execution context.
     /// Can also register channel subscription status. Used to cancel them all at once when the fiber is destroyed.
     /// </summary>
     public interface IFiber : IExecutionContextWithPossibleStoppage, IDisposable, ISubscriptionRegistryViewing
