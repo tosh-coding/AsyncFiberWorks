5f8e35a95435a6fc83d9fdaf7fcdec67e7e6892d
tosh <tosh.coding@gmail.com>
2024-05-02 17:52:37 +0900

    Refactor BlockingCollectionQueue.

--- src/AsyncFiberWorks/Threading/BlockingCollectionQueue.cs
+++ src/AsyncFiberWorks/Threading/BlockingCollectionQueue.cs
@@ -4,17 +4,19 @@ using System.Collections.Concurrent;
 namespace AsyncFiberWorks.Threading
 {
     /// <summary>
-    /// Simple queue for actions.
+    /// Queue for consumer threads. Internally using BlockingCollection class.
     /// </summary>
     public class BlockingCollectionQueue : IDedicatedConsumerThread
     {
-        private readonly object _lock = new object();
+        private readonly object _lockObj = new object();
         private readonly BlockingCollection<Action> _queue = new BlockingCollection<Action>();
 
-        private bool _running = true;
+        private bool _isStarted = false;
+        private bool _canRunning = true;
+        private bool _isDisposed = false;
 
         /// <summary>
-        /// Enqueue a single action.
+        /// Enqueue an action.
         /// </summary>
         /// <param name="action"></param>
         public void Enqueue(Action action)
@@ -22,43 +24,80 @@ namespace AsyncFiberWorks.Threading
             _queue.Add(action);
         }
 
-        private bool IsRunning
+        private bool CanRunning
         {
             get
             {
-                lock (_lock)
+                lock (_lockObj)
                 {
-                    return _running;
+                    return _canRunning;
                 }
             }
         }
 
         /// <summary>
-        /// Execute actions until stopped.
+        /// Start consumption. Continue until stopped.
         /// </summary>
         public void Run()
         {
-            Action action;
-            while (IsRunning)
+            lock (_lockObj)
             {
-                action = _queue.Take();
-                action();
-                while (_queue.TryTake(out action))
+                if (_isDisposed)
                 {
+                    return;
+                }
+                if (_isStarted)
+                {
+                    return;
+                }
+                _isStarted = true;
+            }
+            try
+            {
+                Action action;
+                while (CanRunning)
+                {
+                    action = _queue.Take();
                     action();
+                    while (_queue.TryTake(out action))
+                    {
+                        action();
+                    }
+                }
+            }
+            finally
+            {
+                lock (_lockObj)
+                {
+                    _isDisposed = true;
+                    _canRunning = false;
+                    _queue.Dispose();
                 }
             }
         }
 
         /// <summary>
-        /// Stop consuming the actions.
+        /// Stop consumption.
         /// </summary>
         public void Stop()
         {
-            lock (_lock)
+            lock (_lockObj)
             {
-                _running = false;
-                Enqueue(() => { });
+                if (_isDisposed)
+                {
+                    return;
+                }
+                _isDisposed = true;
+
+                _canRunning = false;
+                if (!_isStarted)
+                {
+                    _queue.Dispose();
+                }
+                else
+                {
+                    Enqueue(() => { });
+                }
             }
         }
     }
