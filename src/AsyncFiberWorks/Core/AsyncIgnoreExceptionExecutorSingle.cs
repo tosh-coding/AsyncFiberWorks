1609bda80cdad850a6b21260575fb6bc494346ea
tosh <tosh.coding@gmail.com>
2024-05-19 17:58:46 +0900

    Add AsyncIgnoreExceptionExecutorSingle.

---
+++ src/AsyncFiberWorks/Core/AsyncIgnoreExceptionExecutorSingle.cs
@@ -0,0 +1,41 @@
+﻿using AsyncFiberWorks.Procedures;
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncFiberWorks.Core
+{
+    /// <summary>
+    /// An executor who ignores exceptions.
+    /// </summary>
+    public class AsyncIgnoreExceptionExecutorSingle : IAsyncExecutorSingle
+    {
+        private Action<Exception> _catchAction;
+
+        /// <summary>
+        /// Create an executor.
+        /// </summary>
+        /// <param name="catchAction">This is the process called when an exception is caught at executing.</param>
+        public AsyncIgnoreExceptionExecutorSingle(Action<Exception> catchAction)
+        {
+            _catchAction = catchAction;
+        }
+
+        /// <summary>
+        /// Start a task and wait for it to finish.
+        /// Exceptions that occur during this time are ignored.
+        /// </summary>
+        /// <param name="func">Target task.</param>
+        /// <returns></returns>
+        public async Task Execute(Func<Task> func)
+        {
+            try
+            {
+                await func();
+            }
+            catch (Exception ex)
+            {
+                _catchAction?.Invoke(ex);
+            }
+        }
+    }
+}
