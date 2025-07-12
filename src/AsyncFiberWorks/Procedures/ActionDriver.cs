ee0307be5956f7893a5e6e37a9bdf51e95fc578b
tosh <tosh.coding@gmail.com>
2025-07-13 02:53:35 +0900

    Delete AsyncTaskQueueLoop.

--- src/AsyncFiberWorks/Procedures/ActionDriver.cs
+++ src/AsyncFiberWorks/Procedures/ActionDriver.cs
@@ -1,6 +1,9 @@
 using AsyncFiberWorks.Core;
+using AsyncFiberWorks.Executors;
+using AsyncFiberWorks.Threading;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AsyncFiberWorks.Procedures
 {
@@ -11,12 +14,26 @@ namespace AsyncFiberWorks.Procedures
     {
         private readonly object _lock = new object();
         private readonly LinkedList<Action<FiberExecutionEventArgs>> _actions = new LinkedList<Action<FiberExecutionEventArgs>>();
-        private readonly AsyncTaskQueueLoop _fiber = new AsyncTaskQueueLoop();
+        private readonly object _lockObj = new object();
+        private readonly Queue<Func<Task>> _queue = new Queue<Func<Task>>();
+        bool _running = false;
+        IAsyncExecutor _executor;
 
         /// <summary>
         /// Create an action driver.
         /// </summary>
+        /// <param name="executor"></param>
+        public ActionDriver(IAsyncExecutor executor)
+        {
+            _executor = executor;
+        }
+
+        /// <summary>
+        /// Create an action driver.
+        /// </summary>
+        /// <param name="executor"></param>
         public ActionDriver()
+            : this(AsyncSimpleExecutor.Instance)
         {
         }
 
@@ -73,10 +90,10 @@ namespace AsyncFiberWorks.Procedures
             {
                 foreach (var action in _actions)
                 {
-                    _fiber.Enqueue(eventArgs.SourceThread, action);
+                    Enqueue(eventArgs.SourceThread, action);
                 }
             }
-            _fiber.Enqueue(() => eventArgs.Resume());
+            Enqueue(() => eventArgs.Resume());
         }
 
         ///<summary>
@@ -91,6 +108,109 @@ namespace AsyncFiberWorks.Procedures
                     return _actions.Count;
                 }
             }
+        }
+
+        bool TryDequeue(out Func<Task> result)
+        {
+            lock (_lockObj)
+            {
+                if (_queue.Count > 0)
+                {
+                    result = _queue.Dequeue();
+                    return true;
+                }
+            }
+            result = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Start consumption.
+        /// </summary>
+        async void Run()
+        {
+            while (true)
+            {
+                while (TryDequeue(out var func))
+                {
+                    await _executor.Execute(func).ConfigureAwait(false);
+                }
+                lock (_lockObj)
+                {
+                    if (_queue.Count <= 0)
+                    {
+                        _running = false;
+                        return;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Enqueue a task.
+        /// </summary>
+        /// <param name="func">A function that returns a task.</param>
+        void PrivateEnqueue(Func<Task> func)
+        {
+            bool startedNow = false;
+            lock (_lockObj)
+            {
+                _queue.Enqueue(func);
+                if (!_running)
+                {
+                    _running = true;
+                    startedNow = true;
+                }
+            }
+
+            if (startedNow)
+            {
+                Run();
+            }
+        }
+
+        /// <summary>
+        /// Enqueue a single action. It is executed sequentially.
+        /// </summary>
+        /// <param name="action">Action to be executed.</param>
+        void Enqueue(Action action)
+        {
+            PrivateEnqueue(() =>
+            {
+                action();
+                return Task.CompletedTask;
+            });
+        }
+
+        /// <summary>
+        /// Enqueue a single action. It is executed sequentially.
+        /// </summary>
+        /// <param name="threadPool">The execution context for the specified action.</param>
+        /// <param name="action">Action to be executed.</param>
+        void Enqueue(IThreadPool threadPool, Action<FiberExecutionEventArgs> action)
+        {
+            PrivateEnqueue(async () =>
+            {
+                await threadPool.SwitchTo();
+                bool isPaused = false;
+                var tcs = new TaskCompletionSource<int>();
+                var eventArgs = new FiberExecutionEventArgs(
+                    () =>
+                    {
+                        isPaused = true;
+                    },
+                    () =>
+                    {
+                        tcs.SetResult(0);
+                    },
+                    DefaultThreadPool.Instance);
+                action(eventArgs);
+                if (isPaused)
+                {
+                    await tcs.Task.ConfigureAwait(false);
+                    await Task.Yield();
+                }
+            });
         }
     }
 }
